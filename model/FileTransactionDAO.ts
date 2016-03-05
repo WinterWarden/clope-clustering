5a0ee64b35d3f1d1d74238cde1c0b301141f8822
Pavel_Smirnov <pavel_smirnov@epam.com>
2016-03-05 17:58:41 +0300

    Begining of FS implementation

--- model/FileTransactionDAO.ts
+++ model/FileTransactionDAO.ts
@@ -1,26 +1,31 @@
-/// <reference path="Model.ts"/>
-module  Model {
+ module  model {
     export class FileTransactionDAO implements TransactionDAO {
-        private transactions:Transaction[];
+        private transactions:{[key:number]: Transaction};
+        private idsCounter: number;
 
         constructor() {
-
+            this.idsCounter = 1;
+            this.transactions = this.parseTransactions();
         }
 
         getTransactionById(transactionId:number):Transaction {
-            return null;
+            return this.transactions[transactionId];
         }
 
         saveTransaction(transaction:Transaction):Transaction {
             if (transaction.id) {
                 this.transactions[transaction.id] = transaction;
             } else {
-                transaction.id = this.transactions.length;
-                this.transactions.push(transaction)
+                transaction.id =  this.idsCounter++;
+                this.transactions[this.idsCounter] = transaction;
             }
             return transaction;
         }
 
+        private parseTransactions():{[key:number]: Transaction} {
+            var transactions:{[key:number]: Transaction} = {};
 
+            return transactions;
+        }
     }
 }
